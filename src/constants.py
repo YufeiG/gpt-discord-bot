97cd2216bcaed5ee810bcb6c0ce300896b4085c2
Yufei Guo <yufei@openai.com>
2023-01-19 14:06:21 -0500

    Fix again

--- src/constants.py
+++ src/constants.py
@@ -25,22 +25,22 @@ BOT_INVITE_URL = f"https://discord.com/api/oauth2/authorize?client_id={DISCORD_C
 
 MODERATION_VALUES_FOR_BLOCKED = {
     "hate": 0.5,
-    "hate/threatening": 0.3,
+    "hate/threatening": 0.4,
     "self-harm": 0.2,
     "sexual": 0.5,
     "sexual/minors": 0.2,
     "violence": 0.95,
-    "violence/graphic": 0.9,
+    "violence/graphic": 0.95,
 }
 
 MODERATION_VALUES_FOR_FLAGGED = {
     "hate": 0.4,
-    "hate/threatening": 0.05,
+    "hate/threatening": 0.2,
     "self-harm": 0.1,
     "sexual": 0.3,
     "sexual/minors": 0.1,
-    "violence": 0.05,
-    "violence/graphic": 0.05,
+    "violence": 0.5,
+    "violence/graphic": 0.5,
 }
 
 SECONDS_DELAY_RECEIVING_MSG = (
